75efa74814399c5fb4e9953f5b60e212715e4521
smidu <kontakt@smid.pl>
2022-05-05 20:41:10 +0200

    php7-nginx-alpine-3.12

--- php7-nginx-alpine-3.12/buildAndPush.sh
+++ php7-nginx-alpine-3.12/buildAndPush.sh
@@ -3,6 +3,5 @@
 REPO=smidu/php7-nginx-alpine-3.12
 VERSION=$(git log -1 --pretty=%H)
 
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t $REPO:$VERSION .
-# docker buildx build --platform linux/amd64 -t $REPO:$VERSION .
+docker build -t $REPO:$VERSION .
 docker push $REPO:$VERSION
