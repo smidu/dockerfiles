0cb51f2a01f8135b8af6646b9fe83240d2ea80a1
smidu <kontakt@smid.pl>
2022-07-07 12:24:19 +0200

    short hash repo version

--- php7-nginx-alpine-3.12/buildAndPush.sh
+++ php7-nginx-alpine-3.12/buildAndPush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPO=smidu/php7-nginx-alpine-3.12
-VERSION=$(git log -1 --pretty=%H)
+VERSION=$(git log -1 --pretty=%h)
 
 docker build -t $REPO:$VERSION .
 docker push $REPO:$VERSION
